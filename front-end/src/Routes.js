dfa413a783eb6df8851b71c8c714af66a2eb984c
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2019-11-04 19:28:29 +0530

    added detail page and also added it's route to the app

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Route, Switch,  } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Home from "./components/Home/home";
 import AddCity from "./components/AddCity/addCity";
+import CityDetail from './components/cityDetail/cityDetail';
 
 function Routes(mainProps) {
   return (
@@ -17,6 +18,12 @@ function Routes(mainProps) {
         path="/add"
         render={props => <AddCity {...props} {...mainProps} />}
       ></Route>
+      <Route
+        exact
+        path="/detail"
+        render={props => <CityDetail {...props} {...mainProps} />}
+      ></Route>
+      <Redirect to="/" />
     </Switch>
   );
 }
---
+++ front-end/src/components/cityDetail/cityDetail.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CityDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm City Component</h1>
+      </div>
+    )
+  }
+}
+
+export default CityDetail;
